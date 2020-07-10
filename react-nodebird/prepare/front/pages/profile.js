2de15373da2fe65ccdbc52416495a2ea486041db
minimani97 <shineeworld408@gmail.com>
2020-07-10 15:46:59 +0900

    프로필 페이지 완성

--- react-nodebird/prepare/front/pages/profile.js
+++ react-nodebird/prepare/front/pages/profile.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
 
+import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
+
 const Profile = () => {
+    const followerList = [{ nickname: '제로초' }, { nickname: '이재욱' }, { nickname: 'NodeBird Official'}];
+    const followingList = [{ nickname: '제로초' }, { nickname: '이재욱' }, { nickname: 'NodeBird Official'}];
+
     return (
         <>
             <Head>
                 <title>내 프로필 | NodeBird</title>
             </Head>
             <AppLayout>
-                <div>내 프로필</div>
+                <NicknameEditForm />
+                <FollowList header="팔로잉" data={followingList} />
+                <FollowList header="팔로워" data={followerList} />
             </AppLayout>
         </>
     );
