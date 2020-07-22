14c8bad8cdaba102897c99bffb43c3ffb8108f0c
minimani97 <shineeworld408@gmail.com>
2020-07-22 17:15:53 +0900

    NodeBird - Infinite Scrolling 구현

--- react-nodebird/prepare/front/pages/index.js
+++ react-nodebird/prepare/front/pages/index.js
@@ -1,14 +1,42 @@
 // next에서 자동으로 넣어주기 때문에 넣어도 되고 안 넣어도 됨!
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import AppLayout from '../components/AppLayout';
 import PostForm from '../components/PostForm';
 import PostCard from '../components/PostCard';
+import { LOAD_POSTS_REQUEST } from '../reducers/post';
 
 const Home = () => {
+    const dispatch = useDispatch();
+
     const { me } = useSelector((state) => state.user);
-    const { mainPosts } = useSelector((state) => state.post);
+    const { mainPosts, hasMorePosts, loadPostsLoading } = useSelector((state) => state.post);
+
+    useEffect(() => {
+        dispatch({
+            type: LOAD_POSTS_REQUEST,
+        });
+    }, []);
+
+    useEffect(() => {
+        function onScroll() {
+            // scrollY : 얼마나 내렸는지, clientHeight: 현재 화면의 세로 길이, scrollHeight: 총 길이
+            if (window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 400) {
+                if (hasMorePosts && !loadPostsLoading) {
+                    dispatch({
+                        type: LOAD_POSTS_REQUEST,
+                    });
+                }
+            }
+        }
+
+        window.addEventListener('scroll', onScroll);
+
+        return () => {
+            window.removeEventListener('scroll', onScroll);
+        };
+    }, [mainPosts, hasMorePosts, loadPostsLoading]);
 
     return (
         <AppLayout>
--- react-nodebird/prepare/front/sagas/post.js
+++ react-nodebird/prepare/front/sagas/post.js
@@ -1,4 +1,4 @@
-import { all, put, fork, takeLatest, delay } from 'redux-saga/effects';
+import { all, put, fork, takeLatest, delay, throttle } from 'redux-saga/effects';
 import axios from 'axios';
 import shortId from 'shortid';
 
@@ -7,7 +7,28 @@ import {
     ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE,
     REMOVE_POST_REQUEST, REMOVE_POST_SUCCESS, REMOVE_POST_FAILURE,
     ADD_POST_TO_ME, REMOVE_POST_OF_ME,
+    LOAD_POSTS_REQUEST, LOAD_POSTS_SUCCESS, LOAD_POSTS_FAILURE, generateDummyPost,
 } from '../reducers/post';
+
+function loadPostsAPI(data) {
+    return axios.get('/api/posts', data);
+}
+
+function* loadPosts() {
+    try {
+        // const result = yield call(loadPostsAPI, action.data);
+        yield delay(1000);
+        yield put({
+            type: LOAD_POSTS_SUCCESS,
+            data: generateDummyPost(10),
+        });
+    } catch (err) {
+        yield put({
+            type: LOAD_POSTS_FAILURE,
+            data: err.response.data,
+        });
+    }
+}
 
 function addPostAPI(data) {
     return axios.post('/api/post', data);
@@ -22,7 +43,7 @@ function* addPost(action) {
             type: ADD_POST_SUCCESS,
             data: {
                 id,
-                content: action.data
+                content: action.data,
             },
         });
         yield put({
@@ -71,7 +92,7 @@ function* addComment(action) {
         yield delay(1000);
         yield put({
             type: ADD_COMMENT_SUCCESS,
-            data: action.data
+            data: action.data,
         });
     } catch (err) {
         yield put({
@@ -79,6 +100,10 @@ function* addComment(action) {
             data: err.response.data,
         });
     }
+}
+
+function* watchLoadPosts() {
+    yield throttle(5000, LOAD_POSTS_REQUEST, loadPosts);
 }
 
 function* watchAddPost() {
@@ -95,6 +120,7 @@ function* watchAddComment() {
 
 export default function* postSaga() {
     yield all([
+        fork(watchLoadPosts),
         fork(watchAddPost),
         fork(watchRemovePost),
         fork(watchAddComment),
